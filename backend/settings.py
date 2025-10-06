a684048e4729698840ec2fc7b09f01c330005ce8
Pstas <ballabtnd@gmail.com>
2025-10-06 07:58:12 +0200

    fix: Update STATIC_URL assignment to use local_settings for DEBUG check

--- backend/settings.py
+++ backend/settings.py
@@ -146,7 +146,7 @@ USE_TZ = False # False, mert csak egy időzónát használunk
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-if DEBUG:
+if local_settings.DJANGO_DEBUG == True:
     STATIC_URL = '/static/'
 else:
     STATIC_URL = 'https://node.szlg.info/__szlg/static/'
